05b282807fc216823fc4322e15878cc135eaedd7
Panithan Lertsuntivit <panithanblue@gmail.com>
2025-09-04 02:18:00 -0700

    Early Fall Update
    Shows last update for website. Included NTHU and resume.

---
+++ scripts/update-date.js
@@ -0,0 +1,8 @@
+// scripts/update-date.js
+// For date updates for the website
+
+// Manually setting the update date
+const lastUpdatedDate = "September 2025";
+
+// Code finds the 'last-updated' span and inserts the date
+document.getElementById('last-updated').textContent = lastUpdatedDate;
