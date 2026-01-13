dadf67a8c880c3a271e161b18c3247f6ee5b6644
Panithan Lertsuntivit <panithanblue@gmail.com>
2026-01-13 15:58:55 -0800

    Update update-date.js

--- scripts/update-date.js
+++ scripts/update-date.js
@@ -1,8 +1,8 @@
 // scripts/update-date.js
-// For date updates for the website
+// Purpose: For date updates for the website
 
 // Manually setting the update date
-const lastUpdatedDate = "September 2025";
+const lastUpdatedDate = "January 2026";
 
 // Code finds the 'last-updated' span and inserts the date
 document.getElementById('last-updated').textContent = lastUpdatedDate;
